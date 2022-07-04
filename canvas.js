48f608eede8ec4b0c1cf7f84984da7d0c70bb1c1
agusgonz <agustingendeiza@gmail.com>
2022-07-04 20:16:09 -0300

    Random

---
+++ canvas.js
@@ -0,0 +1,60 @@
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+
+function dibujar(){
+    for(i = 0; i < 100; i++){
+
+        // Circle
+        var x = Math.random()* window.innerWidth;
+        var y = Math.random()* window.innerHeight;
+        var radius = Math.random()*20;
+    
+        var r = Math.floor(Math.random()*255);
+        var g = Math.floor(Math.random()*255);
+        var b = Math.floor(Math.random()*255);
+      
+        c.beginPath();
+        c.arc(x,y,radius,Math.PI*2,0,false);
+        c.fillStyle = "rgba(" + r + "," + g + "," + b + ",1)";
+        c.fill();
+        c.closePath();
+
+        //Line
+        var x2 = Math.random()* window.innerWidth;
+        var y2 = Math.random()* window.innerHeight;
+        var r1 = Math.floor(Math.random()*255);
+        var g1 = Math.floor(Math.random()*255);
+        var b1 = Math.floor(Math.random()*255);
+        var l = Math.random()*5
+        c.beginPath();
+        c.moveTo(x2 - 1, y2 - 1);
+        c.lineTo(x2 + 1, y2 +1);
+        c.strokeStyle = "rgba(" + r1 + "," + g1 + "," + b1 + ",1)";
+        c.lineWidth = l;
+        c.stroke();
+
+        //Rectangle
+        var x3 = Math.random()* window.innerWidth;
+        var y3 = Math.random()* window.innerHeight;
+    
+        var r2 = Math.floor(Math.random()*255);
+        var g2 = Math.floor(Math.random()*255);
+        var b2 = Math.floor(Math.random()*255);
+
+        var t1 = Math.random()*50;
+        var t2 = Math.random()*50;
+      
+        c.beginPath();
+        c.closePath();
+        c.fillStyle = "rgba(" + r2 + "," + g2 + "," + b2 + ",1)";
+        c.fillRect(x3, y3, t1, t2);
+
+    }
+}
+
+function limpiar(){
+    c.clearRect(0, 0, canvas.width, canvas.width);
+}
